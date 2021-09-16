efa63b29f4fc968fddbc002f18887e867cca2085
Ghostyy 5668 <cosvicx9126@gmail.com>
2021-09-16 18:31:24 +0530

    Add: Starboard Event

---
+++ Events/Starboard.py
@@ -0,0 +1,28 @@
+# Library Imports
+import nextcord, json
+from nextcord.ext import commands
+
+# Custom Imports
+from Functions.Embed import *
+
+# Options from Json
+with open('Config/Options.json') as RawOptions:
+    Options = json.load(RawOptions)
+
+# Starboard Class
+class Starboard(commands.Cog):
+    def __init__(self, bot:commands.Bot):
+        self.bot = bot
+
+    
+    @commands.Cog.listener('on_raw_reaction_add')
+    async def  onReactionAdd(self, payload:nextcord.RawReactionActionEvent):
+        """Triggered when a reaction is added"""
+        channel = self.bot.get_channel(payload.channel_id)
+        partialmessage = channel.get_partial_message(payload.message_id)
+        
+
+
+# Setup the bot
+def setup(bot:commands.Bot):
+    bot.add_cog(Starboard(bot))
