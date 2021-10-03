f83a83679bd2edd587743012b715feb0fb228447
Ghostyy 5668 <cosvicx9126@gmail.com>
2021-10-03 16:51:01 +0530

    Leaderboard command

--- Commands/Rep.py
+++ Commands/Rep.py
@@ -48,13 +48,62 @@ class Rep(commands.Cog):
         # Construct the profile embed
         embed = await Custom(
             f"{user}",
-            f"Your reputation on this server is: **{Profile[1]}**!\nUpvotes: **{Profile[2]}**\nDownvotes : **{Profile[3]}**"
+            f"Your reputation on this server is: **{Profile[1]}\n**!\nUpvotes: **{Profile[2]}**\nDownvotes : **{Profile[3]}**"
         )   
         embed.set_thumbnail(url = user.avatar.url)
 
         # Send the profile embed
         await ctx.send(embed = embed)
 
+        # Close and commit everything
+        Cur.close()
+        db.commit()
+        db.close()
+
+
+    @commands.command(name = "Leaderboard", aliases = ["Lb"])
+    @commands.cooldown(1, 10, commands.BucketType.member)
+    async def  leaderboard(self, ctx: commands.Context, users:int = 10):
+        """The rep leaderboard"""
+        await ctx.channel.trigger_typing()
+        
+        # Limit users to 100
+        if users > 100:
+            users = 100
+        
+        # Connect to database
+        db = mysql.connector.connect(
+            host = str(os.getenv("Host")),
+            user = "ghostyy",
+            passwd = str(os.getenv("Password"))
+        )
+
+        # Get cursor and attempt to get reputation
+        Cur = db.cursor(buffered = True)
+        Cur.execute("SELECT * FROM `thecloud`.`reputation` ORDER BY `Reputation` DESC")
+        Profiles = Cur.fetchmany(users)
+
+        # Build the embed
+        repstring, count = '', 1
+        for Profile in Profiles:
+            User = ctx.guild.get_member(Profile[0])
+            if not User: pass
+            else:
+                if count == 1: repstring += f'🥇 **{count}.**  {User.name} : {Profile[1]}\n'
+                elif count == 2: repstring += f'🥈 **{count}.**  {User.name} : {Profile[1]}\n'
+                elif count == 3: repstring += f'🥉 **{count}.**  {User.name} : {Profile[1]}\n'
+                else: repstring += f'**{count}.**  {User.name} : {Profile[1]}\n'
+                count += 1
+        
+        embed = await Custom(title = f'Top {users} Leaderboard', description = repstring)
+
+        # Send the profile embed
+        await ctx.send(embed = embed)
+
+        # Close and commit everything
+        Cur.close()
+        db.commit()
+        db.close()
 
 # Setup the bot
 def setup(bot:commands.Bot):
